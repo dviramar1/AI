d42e1c8bf4ffe291959757e9e6f5f343c3c7c92e
Dvir Amar <dviramar97@gmail.com>
2022-05-09 23:13:18 +0300

    Added propositions and actions for hanoi domain file

--- ex3/hanoi.py
+++ ex3/hanoi.py
@@ -4,8 +4,27 @@ import sys
 def create_domain_file(domain_file_name, n_, m_):
     disks = ['d_%s' % i for i in list(range(n_))]  # [d_0,..., d_(n_ - 1)]
     pegs = ['p_%s' % i for i in list(range(m_))]  # [p_0,..., p_(m_ - 1)]
+    elements = disks + pegs
+
+    # disk is on an element (on disk or directly on peg)
+    on_propositions = [f"{disk}_on_{element}" for disk, element in zip(disks, elements)]
+    # is disk on top of some peg
+    top_propositions = [f"{element}_top" for element in elements]
+    # is disk smaller than other disk (disk always smaller than peg)
+    smaller_than_propositions = [f"{disk}_st_{element}" for disk, element in zip(disks, elements)]
+    propositions = on_propositions + top_propositions + smaller_than_propositions
+
+    actions = []
+    for disk in disks:
+        for src in elements:
+            for dest in elements:
+                action_name = f"move_{disk}_from_{src}_to_{dest}"
+                action_pre = [f"{disk}_top", f"{dest}_top", f"{disk}_st_{dest}"]
+                action_add = [f"{disk}_on_{dest}", f"{src}_top"]
+                action_delete = [f"{dest}_top", f"{disk}_on_{src}"]
+                actions.append((action_name, action_pre, action_add, action_delete))
+
     domain_file = open(domain_file_name, 'w')  # use domain_file.write(str) to write to domain_file
-    "*** YOUR CODE HERE ***"
 
     domain_file.close()
 
